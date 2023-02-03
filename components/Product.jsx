4f6919f072f22bdbcd1f95588d161e78f140d852
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-02-03 18:08:58 +0100

    implement product component

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,6 +1,25 @@
-const Product = () => {
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+
+import { urlFor } from "../lib/client";
+
+const Product = ({ product: {image, name, slug, price} }) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}>
+          <div className="product-card">
+            <img 
+              src={urlFor(image && image[0])}
+              width={250}
+              height={250}
+              className='product-image'
+            />
+            <p className="product-name">{name}</p>
+            <p className="product-price">{price}€</p>
+          </div>
+      </Link>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,11 @@ export default function Home({products, bannerData}) {
 
       <div className="products-container">
         {
-          products?.map((product) => 
-          product.name)
+          products?.map((product) => <Product key={product._id} product={product}/>)
         }
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </>
   )
 };
