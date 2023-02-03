18e6848a4be6d7b3c5378d90da8e2c6836d3754b
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-02-03 16:21:18 +0100

    hook up sanity

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import {Product, FooterBanner, HeroBanner} from '../components';
+import { client } from '../lib/client';
 
-export default function Home() {
+export default function Home({products, bannerData}) {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
@@ -12,12 +13,23 @@ export default function Home() {
 
       <div className="products-container">
         {
-          ['Product 1', 'Product 2'].map((product) => 
-          product)
+          products?.map((product) => 
+          product.name)
         }
       </div>
 
       <FooterBanner />
     </>
   )
-}
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {products, bannerData}
+  }
+} 
